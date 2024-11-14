9f4cd0d69d58ce73c25c090a2d78bd70e8e494c3
REVE <aiwork4242@gmail.com>
2024-11-14 17:46:24 +0900

    build KosaSecondProjectBackendApplication setting #1

--- src/main/java/com/example/kosa_second_project_backend/KosaSecondProjectBackendApplication.java
+++ src/main/java/com/example/kosa_second_project_backend/KosaSecondProjectBackendApplication.java
@@ -2,10 +2,14 @@ package com.example.kosa_second_project_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) // 스프링 시큐리티 의존성 무시
-public class KosaSecondProjectBackendApplication {
+@EnableJpaRepositories(basePackages = {"com.example.kosa_second_project_backend.repository"})
+@EntityScan(basePackages = {"com.example.kosa_second_project_backend.model.entity"})public class KosaSecondProjectBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KosaSecondProjectBackendApplication.class, args);

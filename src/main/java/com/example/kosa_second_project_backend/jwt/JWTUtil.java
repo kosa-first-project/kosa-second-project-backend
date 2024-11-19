506e800cbfde63fc51097e3e68f969a0d1f5bb25
KimDoHun <thurr1394@gamil.com>
2024-11-19 16:11:07 +0900

    2024-11-19 김도훈

--- src/main/java/com/example/kosa_second_project_backend/jwt/JWTUtil.java
+++ src/main/java/com/example/kosa_second_project_backend/jwt/JWTUtil.java
@@ -5,10 +5,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
